0408dc8d6115ecd5a406ee5982d2a54d5f98c03f
damar-nur-ichwan <damarnurichwan.official@gmail.com>
2022-04-11 12:40:04 +0700

    finish

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-module.exports = {
-    tracer : require('./tracer'),
-    axios: require('./client/axios')
-}
+// // Not functionally required but gives some insight what happens behind the scenes
+// const { diag, DiagConsoleLogger, DiagLogLevel } = opentelemetry;
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+module.exports = function (){
+
+    require('dotenv').config()
+  
+    const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+    const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+    const { Resource } = require('@opentelemetry/resources');
+    const { SemanticResourceAttributes, SemanticAttributes } = require('@opentelemetry/semantic-conventions');
+    const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+    const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+    const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+    const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+    const { AlwaysOnSampler } = require('@opentelemetry/core');
+    const opentelemetry = require('@opentelemetry/api');
+  
+    //set exporter
+    const exporter = (process.env.EXPORTER || '').toLowerCase().startsWith('zipkin') 
+    ? new ZipkinExporter({
+        url: process.env.EXPORTER_URL+'/api/v2/spans',
+        serviceName: process.env.INSTANCE_NAME,
+    }) : new JaegerExporter({
+        serviceName: process.env.INSTANCE_NAME,
+    });
+  
+    //set & regist provider
+    const provider = new NodeTracerProvider({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: process.env.INSTANCE_NAME,
+    }),
+    sampler: filterSampler(ignoreHealthCheck, new AlwaysOnSampler()),
+    });
+    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+    provider.register();
+  
+    //regist instrumentation
+    registerInstrumentations({
+        tracerProvider: provider,
+        instrumentations: [getNodeAutoInstrumentations()]
+    });
+  
+    function filterSampler(filterFn, parent) {
+      return {
+        shouldSample(ctx, tid, spanName, spanKind, attr, links) {
+          if (!filterFn(spanName, spanKind, attr)) {
+            return { decision: opentelemetry.SamplingDecision.NOT_RECORD };
+          }
+          return parent.shouldSample(ctx, tid, spanName, spanKind, attr, links);
+        },
+        toString() {
+          return `FilterSampler(${parent.toString()})`;
+        }
+      }
+    }
+    
+    function ignoreHealthCheck(spanName, spanKind, attributes) {
+      return spanKind !== opentelemetry.SpanKind.SERVER || attributes[SemanticAttributes.HTTP_ROUTE] !== "/health";
+    }
+  
+    return opentelemetry.trace.getTracer(require('./package.json')['name'])
+  }
+  
+  
